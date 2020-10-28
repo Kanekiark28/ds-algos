475e36ede43f220f240cc939820acba55078c6da
Kanekiark28 <konarkrajmishra@gmail.com>
2020-10-27 18:22:31 -0700

    trees done

--- trees-recursion/inorder.py
+++ trees-recursion/inorder.py
@@ -3,6 +3,8 @@ Inorder traversal of BST
 Order: LNR
 """
 
+from collections import deque
+
 class Node:
     
     def __init__(self,val):
@@ -39,15 +41,122 @@ class Node:
             self.recurse(root.left,res)
             res.append(root.val)
             self.recurse(root.right,res)
-            
-root = Node(27)
-root.insert(14)
-root.insert(35)
-root.insert(10)
-root.insert(19)
-root.insert(31)
-root.insert(42)
-print(root.inorder(root))
+    
+    def max_depth(self,root):
+        if not root:
+            return 0
+        
+        leftHeight = self.max_depth(root.left)
+        rightHeight = self.max_depth(root.right)
+        
+        return max(leftHeight,rightHeight)+1
+    
+    def isSymmetric(self, root):
+        #if not root == isSymmetric!
+        if not root:
+            return True
+        
+        return self.isSymmetricLeftRight(root.left,root.right)
+    
+    def isSymmetricLeftRight(self,left,right):
+        #check if both are none or tree is unbalanced
+        if not left or not right:
+            return left == right
+        #if left == right check if left.left == right.right
+        elif left.val == right.val and self.isSymmetricLeftRight(left.left,right.right) and self.isSymmetricLeftRight(left.right,right.left):
+            return True
+        
+    def isSameTree(self,p,q):
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right):
+            return True
+        
+    def diameterOfBinaryTree(self, root):
+        if not root:
+            return 0
+        
+        lHeight = self.max_depth(root.left)
+        rHeight = self.max_depth(root.right)
+        lDiameter = self.diameterOfBinaryTree(root.left)
+        rDiameter = self.diameterOfBinaryTree(root.right)
+        
+        return max((lHeight+rHeight),max(lDiameter,rDiameter))
+    
+    def validateBST(self,root):
+        return self.validate(root,None,None)
+    
+    def validate(self,root,minimum,maximum):
+        if not root:
+            return True
+        elif not minimum and root.val<=minimum or not maximum and root.val >= maximum:
+            return False
+        else:
+            return self.validate(root.left,minimum,root.val) and self.validate(root.right,root.val,maximum)
+    
+    def levelOrder(self,root):
+        res = []
+        if not root:
+            return res
+        q = deque()
+        q.append(root)
+        while q:
+            size = len(q)
+            currlevel = []
+            for i in range(size):
+                item = q.popleft()
+                currlevel.append(item.val)
+                if item.left:
+                    q.append(item.left)
+                if item.right:
+                    q.append(item.right)
+            res.append(currlevel)
+        return res
+    
+    def rightSidedView(self,root):
+        res = []
+        if not root:
+            return res
+        
+        q = deque()
+        q.append(root)
+        while q:
+            size = len(q)
+            for i in range(size):
+                curr = q.popleft()
+                if i == size - 1:
+                    res.append(curr.val)
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+        
+        return res
+    
+    def kthSmallest(self,root,k):
+        res = []
+        self.kinorder(root,k,res)
+        return res[k-1]
+    
+    def kinorder(self,root,k,res):
+        if root:
+            self.kinorder(root.left,k,res)
+            res.append(root.val)
+            self.kinorder(root.right,k,res)
+
+    
+arr = [1,2,3,5,4]
+root = Node(1)
+root.right = Node(3)
+root.left = Node(2)
+root.left.right = Node(5)
+root.right.right = Node(4)
+print(root.rightSidedView(root))
+    
+        
+
 
         
                 
